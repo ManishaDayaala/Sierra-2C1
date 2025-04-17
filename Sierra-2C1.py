9340653459204be4d94c4d41053a2e67f3d58ace
ManishaDayaala <Manishadayaala@gmail.com>
2025-04-17 17:02:05 +0530

    Update Sierra-2C1.py

--- Sierra-2C1.py
+++ Sierra-2C1.py
@@ -26,7 +26,7 @@ def set_random_seed(seed_value=42):
     tf.random.set_seed(seed_value)
 
 # Define the main folder path
-MAINFOLDER = r"D:\APPdata_Sierra-2C1"
+MAINFOLDER = r"./APPdata_Sierra-2C1"
 
 # Create other paths relative to the main folder
 training_file_path = os.path.join(MAINFOLDER, "Training", "Training_Sierra-2C1.xlsx")  # FIXED TRAINING DATA
